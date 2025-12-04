2d780278181abc272cda881f28efbdf8e1fd8ff0
AHAMMED FAVAZ K M <favazmubarak25@gmail.com>
2025-12-04 21:55:06 +0530

    December 04 night

--- app/page.tsx
+++ app/page.tsx
@@ -5,33 +5,41 @@ import { HeroSection } from "@/components/HeroSection";
 import { Navbar } from "@/components/Navbar";
 import PortfolioPage from "@/components/ProjectSection";
 import React from "react";
-// import Silk from "../components/Silk";
-import LightRays from '../components/LightRays';
+import LightRays from "../components/LightRays";
+import ClickSpark from "../components/ClickSpark";
 
 export default function Home() {
   return (
     <div className="w-full min-h-screen">
+       <ClickSpark
+            sparkColor="#fff"
+            sparkSize={10}
+            sparkRadius={25}
+            sparkCount={8}
+            duration={300}
+          >
+            {/* Your content here */}
+          
       {/* HERO WITH STICKY BACKGROUND IMAGE */}
       <section className="relative w-full h-screen overflow-hidden">
         <div className="absolute w-full h-screen">
-  <LightRays
-    raysOrigin="top-center"
-    raysColor="#0fffff"
-    raysSpeed={0.3}
-    lightSpread={0.3}
-    rayLength={2}
-    followMouse={true}
-    mouseInfluence={0.1}
-    noiseAmount={0}
-    distortion={0}
-    className="custom-rays"
-  />
-</div>
+          <LightRays
+            raysOrigin="top-center"
+            raysColor="#0fffff"
+            raysSpeed={0.3}
+            lightSpread={0.3}
+            rayLength={2}
+            followMouse={true}
+            mouseInfluence={0.3}
+            noiseAmount={0}
+            distortion={0}
+            className="custom-rays"
+          />
+        </div>
 
         {/* Hero Content */}
         <div className="relative z-10 flex flex-col h-full px-6 md:px-12">
           <Navbar />
-
           <div className="flex flex-col justify-between flex-grow mt-30">
             <HeroSection />
           </div>
@@ -44,6 +52,7 @@ export default function Home() {
         <PortfolioPage />
         <ContactPage />
       </div>
+      </ClickSpark>
     </div>
   );
 }
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,61 +1,70 @@
 import React from "react";
-// import Silk from './Silk';
+import TextType from './TextType';
 
 export function HeroSection() {
   return (
     <section className="w-full mt-20 text-left text-white hidden sm:block">
       <div className="flex gap-2 ">
-        <h1 className="text-2xl font_new">hello,</h1>
+        <h1 className="text-2xl font_science font-light">Hello, I am</h1>
+      </div>
+      
+      <div className="flex gap-5 font-extrabold ">
+        {/* <h1 className="text-5xl leading-tight font_science">I'm</h1> */}
+        {/* <h1 className="font_science text-cyan-400 text-5xl leading-tight tracking-tighter font-light ">
+          I am
+        </h1> */}
+        <h1 className=" font_Science text-cyan-400 text-5xl leading-tight  ">
+         ______ AHMD
+        </h1>
+           <h1 className="font_Science text-cyan-400  text-5xl leading-tight  ">
+          FAVAZ 
+        </h1>
       </div>
     
-      <h1 className="text-5xl font-bold leading-tight font_science"> I'm a FULL STACK</h1>
-      <p className="mt-3 text-8xl text-cyan-400  font_science "> DEVELOPER </p>
+      {/* <p className="mt-3 text-6xl text-cyan-400 ">SOFTWARE DEVELOPER</p> */}
+<TextType 
+  text={["Software Developer", "UI/UX Designer"]}
+  typingSpeed={75}
+  pauseDuration={1500}
+  showCursor={true}
+  cursorCharacter="_"
+  className="text-7xl font-bold leading-tight tracking-wide text-cyan-100 font_Right"
+/>
       {/* <p className="max-w-xl mx-auto mt-6 text-sm text-gray-300">
         Crafting clean systems, solving messy problems, and turning ideas into something people can actually use.
       </p> */}
       <div className="flex justify-start gap-4 mt-8 ">
-       
- <button
-  className="group inline-flex items-center gap-3 font-semibold rounded-full 
+        <button
+          className="group inline-flex items-center gap-3 font-semibold rounded-full 
          px-6 py-3 text-white bg-transparent cursor-pointer 
          transition-colors duration-300 hover:bg-transparent overflow-hidden"
->
-  <span
-    className="relative w-[25px] h-[25px] grid place-items-center bg-white text-black
+        >
+          <span
+            className="relative w-[25px] h-[25px] grid place-items-center bg-white text-black
            rounded-full overflow-hidden group-hover:bg-black group-hover:text-white"
-  >
-  
-    <svg
-      viewBox="0 0 14 15"
-      fill="currentColor"
-      width="10"
-      className="transition-transform duration-300 group-hover:translate-x-[150%] group-hover:-translate-y-[150%]"
-    >
-      <path
-        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-      />
-    </svg>
+          >
+            <svg
+              viewBox="0 0 14 15"
+              fill="currentColor"
+              width="10"
+              className="transition-transform duration-300 group-hover:translate-x-[150%] group-hover:-translate-y-[150%]"
+            >
+              <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" />
+            </svg>
 
-   
-    <svg
-      viewBox="0 0 14 15"
-      fill="currentColor"
-      width="10"
-      className="absolute transition-transform duration-300 delay-100 
+            <svg
+              viewBox="0 0 14 15"
+              fill="currentColor"
+              width="10"
+              className="absolute transition-transform duration-300 delay-100 
              -translate-x-[150%] translate-y-[150%] 
              group-hover:translate-x-0 group-hover:translate-y-0"
-    >
-      <path
-        d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z"
-      />
-    </svg>
-  </span>
-
-Download CV
-</button>
-
-
-
+            >
+              <path d="M13.376 11.552l-.264-10.44-10.44-.24.024 2.28 6.96-.048L.2 12.56l1.488 1.488 9.432-9.432-.048 6.912 2.304.024z" />
+            </svg>
+          </span>
+          Download CV
+        </button>
       </div>
     </section>
   );
